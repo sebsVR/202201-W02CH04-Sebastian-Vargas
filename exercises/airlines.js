62397a54826a34cc8e5c59c070ad2bfb5106b83c
Sebas Vargas <sebas_10parla@hotmail.com>
2022-01-28 08:42:40 +0100

    Solved bugs issues

--- exercises/airlines.js
+++ exercises/airlines.js
@@ -1,55 +1,103 @@
 function airlines() {
+    let nombreUsuario = prompt('Escriba su nombre, por favor.');
+    let disponibilidad = '';
+    let costeMedio = 0;
+    let escala = 'Los siguientes vuelos realizan escala:\n\n';
+    let ultVuelos = `En este momento los únicos vuelos por efectuarse tienen como destino:\n\n`;
+    let vuelos = [
+        {
+            id: 0,
+            hasta: 'Bilbao',
+            desde: 'Barcelona',
+            coste: 1600,
+            escala: false,
+        },
 
-  let nombreUsuario = prompt('Escriba su nombre, por favor.');
-  let disponibilidad = '';
-	let costeMedio = 0;
-	let escala = 'Los siguientes vuelos realizan escala:\n\n';
-	let ultVuelos = `En este momento los únicos vuelos por efectuarse tienen como destino:\n\n`;
-	let vuelos = [
+        {
+            id: 1,
+            hasta: 'Nueva York',
+            desde: 'Barcelona',
+            coste: 700,
+            escala: false,
+        },
 
-    {	id: 00, hasta: 'Bilbao', desde: 'Barcelona', coste: 1600, escala: false	},
+        {
+            id: 2,
+            hasta: 'Los Ángeles',
+            desde: 'Madrid',
+            coste: 1100,
+            escala: true,
+        },
 
-		{	id: 01, hasta: 'Nueva York', desde: 'Barcelona', coste: 700, escala: false	},
+        {
+            id: 3,
+            hasta: 'París',
+            desde: 'Barcelona',
+            coste: 210,
+            escala: false,
+        },
 
-		{	id: 02, hasta: 'Los Ángeles', desde: 'Madrid', coste: 1100, escala: true	},
+        { id: 4, hasta: 'Roma', desde: 'Barcelona', coste: 150, escala: false },
 
-		{	id: 03, hasta: 'París', desde: 'Barcelona', coste: 210, escala: false	},
+        { id: 5, hasta: 'Londres', desde: 'Madrid', coste: 200, escala: false },
 
-		{	id: 04, hasta: 'Roma', desde: 'Barcelona', coste: 150, escala: false	},
+        {
+            id: 6,
+            hasta: 'Madrid',
+            desde: 'Barcelona',
+            coste: 90,
+            escala: false,
+        },
 
-		{	id: 05, hasta: 'Londres', desde: 'Madrid', coste: 200, escala: false	},
+        { id: 7, hasta: 'Tokio', desde: 'Madrid', coste: 1500, escala: true },
 
-		{	id: 06, hasta: 'Madrid', desde: 'Barcelona', coste: 90, escala: false	},
+        {
+            id: 8,
+            hasta: 'Shanghái',
+            desde: 'Barcelona',
+            coste: 800,
+            escala: true,
+        },
 
-		{	id: 07, hasta: 'Tokio', desde: 'Madrid', coste: 1500, escala: true	},
+        {
+            id: 9,
+            hasta: 'Sídney',
+            desde: 'Barcelona',
+            coste: 150,
+            escala: true,
+        },
+    ];
 
-		{	id: 08, hasta: 'Shanghái', desde: 'Barcelona', coste: 800, escala: true	},
+    for (let i = 0; i < vuelos.length; i++) {
+        disponibilidad += `El vuelo con origen: ${
+            vuelos[i].desde
+        }, y destino: ${vuelos[i].hasta} \
+tiene un coste de ${vuelos[i].coste} € y ${
+            vuelos[i].escala
+                ? 'realiza una escala'
+                : 'no realiza ninguna escala'
+        }.\n\n`;
 
-		{	id: 09, hasta: 'Sídney', desde: 'Barcelona', coste: 150, escala: true	},
+        costeMedio += vuelos[i].coste;
 
-  ]
+        if (vuelos[i].escala) {
+            escala += `	- Origen ${vuelos[i].desde} y destino ${vuelos[i].hasta}.\n\n`;
+        }
 
-	for (let i = 0; i < vuelos.length; i++) {
+        if (i > vuelos.length - 6) {
+            ultVuelos += `	- ${vuelos[i].hasta}.\n\n`;
+        }
+    }
 
-		disponibilidad += `El vuelo con origen: ${vuelos[i].desde}, y destino: ${vuelos[i].hasta} \
-tiene un coste de ${vuelos[i].coste} € y ${vuelos[i].escala ? 'realiza una escala' : 'no realiza ninguna escala'}.\n\n`;
-
-		costeMedio += vuelos[i].coste;
-
-		if (vuelos[i].escala) {escala += `	- Origen ${vuelos[i].desde} y destino ${vuelos[i].hasta}.\n\n`;}
-
-		if (i > vuelos.length -6) {ultVuelos += `	- ${vuelos[i].hasta}.\n\n`;}
-
-	}
-
-	costeMedio = (costeMedio/vuelos.length - 1).toFixed(2);
+    costeMedio = (costeMedio / vuelos.length - 1).toFixed(2);
 
     alert(`Bienvenido, ${nombreUsuario}.`);
-    alert(`La disponibilidad de vuelos para hoy es la siguiente: \n\n${disponibilidad}`)
-		alert(`El coste medio de los vuelos es de ${costeMedio} €.`);
-		alert(escala);
-		alert(ultVuelos);
-
+    alert(
+        `La disponibilidad de vuelos para hoy es la siguiente: \n\n${disponibilidad}`
+    );
+    alert(`El coste medio de los vuelos es de ${costeMedio} €.`);
+    alert(escala);
+    alert(ultVuelos);
 }
 
 airlines();
--- exercises/calculator.js
+++ exercises/calculator.js
@@ -3,37 +3,55 @@ function calculator() {
 
     let num1 = parseFloat(prompt('Escriba el primer número.'));
 
-        if (isNaN(num1)) {
-            console.log('No escribió números.');
-            alert('No escribió un número. Inténtelo de nuevo.');
-            return calculator();
-        } else {
-            console.log(`Primer número: ${num1}.`);
-        }
+    if (isNaN(num1)) {
+        console.log('No escribió números.');
+        alert('No escribió un número. Inténtelo de nuevo.');
+        return calculator();
+    } else {
+        console.log(`Primer número: ${num1}.`);
+    }
 
-     let num2 = parseFloat(prompt('Escriba un segundo número si quiere. \n\Si no, solo pulse aceptar.'));
+    let num2 = parseFloat(
+        prompt(
+            'Escriba un segundo número si quiere. \nSi no, solo pulse aceptar.'
+        )
+    );
 
-        if (isNaN(num2)) {
-            console.log('No escribió un segundo número.');
-            num1 % 2 === 0 || num1 % 2 === 1 ? resultado.push(Math.sqrt(num1)) : resultado.push(Math.sqrt(num1).toFixed(3));
-            resultado[0] % 2 !== 0 || resultado[0] % 2 !== 1 ? resultado[0] = resultado[0].toFixed(3): resultado[0] = resultado[0];
-            console.log(`Resultado: la raíz cuadrada de ${num1} es ${resultado[0]}.`);
-            return alert(`La raíz cuadrada de ${num1} es ${resultado[0]}.`);
-        } else {
-            console.log(`Segundo número: ${num2}.`);
-            let suma = num1 + num2;
-                suma % 2 === 0 || suma % 2 === 1 ? resultado.push(suma) : resultado.push(suma.toFixed(3));
-            let resta = num1 - num2;
-                resta % 2 === 0 || resta % 2 === 1 ? resultado.push(resta) : resultado.push(resta.toFixed(3));
-            let multi = num1 * num2;
-                multi % 2 === 0 || multi % 2 === 1 ? resultado.push(multi) : resultado.push(multi.toFixed(3));
-            let div = num1 / num2;
-                div % 2 === 0 || div % 2 === 1 ? resultado.push(div) : resultado.push(div.toFixed(3));
-            console.log(`Suma de ${num1} y ${num2}: ${resultado[0]}.`);
-            console.log(`Resta de ${num1} y ${num2}: ${resultado[1]}.`);
-            console.log(`Multiplicación de ${num1} y ${num2}: ${resultado[2]}.`);
-            console.log(`División de ${num1} y ${num2}: ${resultado[3]}.`);
+    if (isNaN(num2)) {
+        console.log('No escribió un segundo número.');
+        num1 % 2 === 0 || num1 % 2 === 1
+            ? resultado.push(Math.sqrt(num1))
+            : resultado.push(Math.sqrt(num1).toFixed(3));
+        if (resultado[0] % 2 !== 0 || resultado[0] % 1 !== 1) {
+            resultado[0] = resultado[0].toFixed(2);
         }
+        console.log(
+            `Resultado: la raíz cuadrada de ${num1} es ${resultado[0]}.`
+        );
+        return alert(`La raíz cuadrada de ${num1} es ${resultado[0]}.`);
+    } else {
+        console.log(`Segundo número: ${num2}.`);
+        let suma = num1 + num2;
+        suma % 2 === 0 || suma % 2 === 1
+            ? resultado.push(suma)
+            : resultado.push(suma.toFixed(3));
+        let resta = num1 - num2;
+        resta % 2 === 0 || resta % 2 === 1
+            ? resultado.push(resta)
+            : resultado.push(resta.toFixed(3));
+        let multi = num1 * num2;
+        multi % 2 === 0 || multi % 2 === 1
+            ? resultado.push(multi)
+            : resultado.push(multi.toFixed(3));
+        let div = num1 / num2;
+        div % 2 === 0 || div % 2 === 1
+            ? resultado.push(div)
+            : resultado.push(div.toFixed(3));
+        console.log(`Suma de ${num1} y ${num2}: ${resultado[0]}.`);
+        console.log(`Resta de ${num1} y ${num2}: ${resultado[1]}.`);
+        console.log(`Multiplicación de ${num1} y ${num2}: ${resultado[2]}.`);
+        console.log(`División de ${num1} y ${num2}: ${resultado[3]}.`);
+    }
     return alert(`El resultado de la suma de ${num1} y ${num2} es ${resultado[0]}.
 El resultado de la resta de ${num1} y ${num2} es ${resultado[1]}.
 El resultado de la multiplicación de ${num1} y ${num2} es ${resultado[2]}.
